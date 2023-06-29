481801c1c1c689f97ff87484f224485d10144de7
Kurt Lust <Kurt.Lust@uantwerpen.be>
2023-06-29 19:25:13 +0200

    More or less finished improved hello_jobstep with sorting and showing of the mapping.

--- Courses/Kurt/hello_jobstep/hello_jobstep.cpp
+++ Courses/Kurt/hello_jobstep/hello_jobstep.cpp
@@ -61,6 +61,9 @@ const char * const busid_map_values_long[MAXGPUS] = {
 
 #define L3DOMAINS 8
 
+// MPI 000 - OMP 000 - HWT 001 (CCD0) - Node nid005952 - RT_GPU_ID 0,1,2,3,4,5,6,7 - GPU_ID 0,1,2,3,4,5,6,7 - Bus_ID c1(GCD0/CCD6),c6(GCD1/CCD7),c9(GCD2/CCD2),cc(GCD3/CCD3),d1(GCD4/CCD0),d6(GCD5/CCD1),d9(GCD6/CCD4),dc(GCD7/CCD5)
+#define OUTPUT_LINE_LENGTH 256
+
 unsigned int HWT_to_L3domain( unsigned int HWT ) {
 
 	unsigned int HWT2 = HWT;
@@ -73,6 +76,16 @@ unsigned int HWT_to_L3domain( unsigned int HWT ) {
 }
 
 #endif
+
+typedef struct {
+
+	char *p_IObuf;                   // Will point to the first character of the IO buffer for thread 0.
+
+    int  mpi_myrank;                 // MPI rank of the current process
+    int  openmp_numthreads;          // Number of threads in the current MPI process
+
+    char IObuf[OUTPUT_LINE_LENGTH]; // IO string buffer for the first thread.
+} t_rankData;
 
 // Macro for checking errors in HIP API calls
 #define hipErrorCheck(call)                                                                     \
@@ -107,6 +120,7 @@ void print_help( const char *exe_name ) {
 		"  -h         Show help information and exit\n"
 		"  -l         Shows a bit more information: CCD with the thread number\n"
 		"             and GCD and optimal CCD with the PCIe bus ID\n"
+		"  -u         Unsorted printing, may work in some heterogeneous cases also\n"
 		"\n"
 	);
 
@@ -150,6 +164,14 @@ void print_help( const char *exe_name ) {
 	);
 #endif
 
+	fprintf( stderr,
+			"Restrictions:\n"
+			"\n"
+			"  - This tool assumes that all nodes are of the same type.\n"
+			"  - This tool assumes that all MPI ranks have the same number of threads.\n"
+			"\n"
+		);
+
 	fflush( stderr );
 
 }
@@ -159,7 +181,7 @@ void print_help( const char *exe_name ) {
 //******************************************************************************
 //
 // get_args( argc, argv, int argc, char **argv, int mpi_myrank,
-//		     unsigned int *show_optimap
+//		     unsigned int *show_optimap, unsigned int *unsorted_print )
 //
 // Gets the input arguments.
 //
@@ -174,12 +196,13 @@ void print_help( const char *exe_name ) {
 //
 
 void get_args( int argc, char **argv, int mpi_myrank,
-		       unsigned int *show_optimap ) {
+		       unsigned int *show_optimap, unsigned int *unsorted_print ) {
 
 	char *exe_name;
 
 	// Make sure we always return initialised variables, whatever happens.
 	*show_optimap = 0;
+	*unsorted_print = 0;
 
 	// Remove the program name
 	exe_name = basename( *argv++ );
@@ -192,6 +215,8 @@ void get_args( int argc, char **argv, int mpi_myrank,
 			exit( EXIT_SUCCESS );
 		} else if ( strcmp( *argv, "-l") == 0 ) {
 			*show_optimap = 1;
+		} else if ( strcmp( *argv, "-u") == 0 ) {
+			*unsorted_print = 1;
 		} else {
 			fprintf( stderr, "%s: Illegal flag found: %s\n", exe_name, *argv);
 			exit( EXIT_WRONG_ARGUMENT );
@@ -238,6 +263,7 @@ int get_GCD( const char *busid, const char * const busid_map[] ) {
 int main(int argc, char *argv[]){
 
 	unsigned int show_optimap;
+	unsigned int unsorted_print;
 
 	// Initialize MPI
 
@@ -249,11 +275,18 @@ int main(int argc, char *argv[]){
 	int rank;
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 
+	// Initialize OpenMP threading
+
+	int num_threads;
+    #pragma omp parallel shared( num_threads )   // The shared clause is not strictly needed as that variable will be shared by default.
+    { if ( omp_get_thread_num() == 0 ) num_threads = omp_get_num_threads(); } // Must be in a parallel session to get the proper number.
+
+
 	//
 	// Interpret the command line arguments
     //
 
-    get_args( argc, argv, rank, &show_optimap );
+    get_args( argc, argv, rank, &show_optimap, &unsorted_print );
 
 	//
 	// Data gathering
@@ -306,6 +339,21 @@ int main(int argc, char *argv[]){
 	int thread_id = 0;
 	unsigned int CCD;
 
+	t_rankData *my_rankData;   // Rank data for this MPI process
+	int my_rankData_size;
+
+	//
+	// Create a buffer to prepare the I/O (to have as much joint code for sorted and unsorted)
+	//
+
+	my_rankData_size = sizeof( t_rankData ) + num_threads * OUTPUT_LINE_LENGTH * sizeof( char );
+    my_rankData = (t_rankData *) malloc( my_rankData_size );
+    if ( my_rankData == NULL ) { fprintf( stderr, "ERROR: Memory allocation for my_rankData failed.\n" ); return 1; };
+    my_rankData->p_IObuf = (char *) &(my_rankData->IObuf);
+
+    my_rankData->mpi_myrank = rank;
+    my_rankData->openmp_numthreads = num_threads;
+
 	if ( num_devices == 0 ) {
 		#pragma omp parallel default(shared) private(hwthread, thread_id, CCD)
 		{
@@ -314,16 +362,18 @@ int main(int argc, char *argv[]){
 			CCD = HWT_to_L3domain( (unsigned int) hwthread );
 
 			if (show_optimap )
-				printf( "MPI %03d - OMP %03d - HWT %03d (CCD%d) - Node %s - GPU N/A\n",
-						rank, thread_id, hwthread, CCD, name);
+				sprintf( my_rankData->p_IObuf + thread_id * OUTPUT_LINE_LENGTH,
+						 "MPI %03d - OMP %03d - HWT %03d (CCD%d) - Node %s - GPU N/A",
+						 rank, thread_id, hwthread, CCD, name);
 			else
-				printf( "MPI %03d - OMP %03d - HWT %03d - Node %s - GPU N/A\n",
-						rank, thread_id, hwthread, name);
+				sprintf( my_rankData->p_IObuf + thread_id * OUTPUT_LINE_LENGTH,
+						 "MPI %03d - OMP %03d - HWT %03d - Node %s - GPU N/A",
+						 rank, thread_id, hwthread, name);
 		}
 	} else {
 
-        std::string busid_list = "";
-        std::string rt_gpu_id_list = "";
+		std::string busid_list = "";
+		std::string rt_gpu_id_list = "";
 
 		// Loop over the GPUs available to each MPI rank
 		for(int i=0; i<num_devices; i++) {
@@ -333,42 +383,94 @@ int main(int argc, char *argv[]){
 			if ( GCD < 0 ) {
 				fprintf( stderr, "Unrecognized bus ID '%s' - %s:%d\n", busid_array[i], __FILE__, __LINE__ );
 				exit(0);
-			}
+			} // end if ( GCD < 0 )
 
 			// Concatenate per-MPIrank GPU info into strings for print
-            if (i > 0) rt_gpu_id_list.append( "," );
-            rt_gpu_id_list.append(std::to_string(i));
+			if (i > 0) rt_gpu_id_list.append( "," );
+			rt_gpu_id_list.append(std::to_string(i));
 
-            //std::string temp_busid( busid_array[i] );
+			//std::string temp_busid( busid_array[i] );
 
-            if (i > 0) busid_list.append(",");
-            if ( show_optimap ) {
-            	std::string temp_busid( busid_map_values_long[GCD] );
-                busid_list.append( temp_busid );
-            } else {
-            	std::string temp_busid( busid_map_values_short[GCD] );
-                busid_list.append( temp_busid );
-            }
+			if (i > 0) busid_list.append(",");
+			if ( show_optimap ) {
+				std::string temp_busid( busid_map_values_long[GCD] );
+				busid_list.append( temp_busid );
+			} else {
+				std::string temp_busid( busid_map_values_short[GCD] );
+				busid_list.append( temp_busid );
+			} // end else-part if ( show_optimap )
 
-		}
+		} // end for( int i=0, ... )
 
 		#pragma omp parallel default(shared) private(hwthread, thread_id)
 		{
-            #pragma omp critical
-            {
+			#pragma omp critical
+			{
 			thread_id = omp_get_thread_num();
 			hwthread = sched_getcpu();
 			CCD = HWT_to_L3domain( (unsigned int) hwthread );
 
 			if (show_optimap )
-				printf( "MPI %03d - OMP %03d - HWT %03d (CCD%d) - Node %s - RT_GPU_ID %s - GPU_ID %s - Bus_ID %s\n",
-						rank, thread_id, hwthread, CCD, name, rt_gpu_id_list.c_str(), gpu_id_list, busid_list.c_str() );
+				sprintf( my_rankData->p_IObuf + thread_id * OUTPUT_LINE_LENGTH,
+						 "MPI %03d - OMP %03d - HWT %03d (CCD%d) - Node %s - RT_GPU_ID %s - GPU_ID %s - Bus_ID %s",
+						 rank, thread_id, hwthread, CCD, name, rt_gpu_id_list.c_str(), gpu_id_list, busid_list.c_str() );
 			else
-				printf( "MPI %03d - OMP %03d - HWT %03d - Node %s - RT_GPU_ID %s - GPU_ID %s - Bus_ID %s\n",
-				        rank, thread_id, hwthread, name, rt_gpu_id_list.c_str(), gpu_id_list, busid_list.c_str() );
-           }
-		}
-	}
+				sprintf( my_rankData->p_IObuf + thread_id * OUTPUT_LINE_LENGTH,
+						 "MPI %03d - OMP %03d - HWT %03d - Node %s - RT_GPU_ID %s - GPU_ID %s - Bus_ID %s",
+						 rank, thread_id, hwthread, name, rt_gpu_id_list.c_str(), gpu_id_list, busid_list.c_str() );
+		   } // end #pragma omp critical
+		} // end #pragma omp parallel
+	} // end if (num_devices == 0 )
+
+	//
+	// Now print to stdout, either from each MPI process (unsorted) or
+	// by collecting all output in rank 0.
+	//
+	if ( unsorted_print ) {
+
+		for ( int i=0; i<num_threads; i++)
+			printf( "%s\n", my_rankData->p_IObuf + i*OUTPUT_LINE_LENGTH );
+
+	} else { // else-part of if ( unsorted_print )
+
+		int error;
+
+		t_rankData *buf_rankData;  // Buffer to receive data from another process.
+		int buf_rankData_size;
+
+		const int mssgID = 1;
+	    MPI_Request request;
+	    MPI_Status status;
+
+		// Create the buffer.
+
+		buf_rankData_size = sizeof( t_rankData ) + num_threads * OUTPUT_LINE_LENGTH * sizeof( char );
+        buf_rankData = (t_rankData *) malloc( my_rankData_size );
+        if ( buf_rankData == NULL ) { fprintf( stderr, "ERROR: Memory allocation for buf_rankData failed.\n" ); return 1; };
+        buf_rankData->p_IObuf = (char *) &(buf_rankData->IObuf);
+
+        // Send the data
+
+        error = MPI_Isend( my_rankData, my_rankData_size, MPI_BYTE, 0, mssgID, MPI_COMM_WORLD, &request );
+
+        // On rank 0: Receive the data and print.
+
+        if ( rank == 0 ) {
+
+        	for ( int c1 = 0; c1 < size; c1++ ) { // Loop over all ranks.
+
+        		error = MPI_Recv( buf_rankData, buf_rankData_size, MPI_BYTE, c1, mssgID, MPI_COMM_WORLD, &status );
+                buf_rankData->p_IObuf = (char *) &(buf_rankData->IObuf); // Need to redo this as it is overwritten by the communication.
+
+        		for ( int c2 = 0; c2 < buf_rankData->openmp_numthreads; c2++ )
+        			printf( "%s\n", buf_rankData->p_IObuf + c2*OUTPUT_LINE_LENGTH );
+
+        	}  // end for ( int c1 = 0; c1 < size; c1++ )
+
+
+        }  // end if ( rank == 0 )
+
+	} // end else part if ( unsorted_print )
 
 	MPI_Finalize();
 
