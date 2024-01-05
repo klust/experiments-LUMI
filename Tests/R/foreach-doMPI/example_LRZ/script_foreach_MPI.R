a1155634ebd1e45b40a9e51fb767a25e1e6901f4
Kurt Lust <Kurt.Lust@uantwerpen.be>
2024-01-05 16:48:23 +0100

    Update of the R examples.

--- Tests/R/foreach-doMPI/example_LRZ/script_foreach_MPI.R
+++ Tests/R/foreach-doMPI/example_LRZ/script_foreach_MPI.R
@@ -5,7 +5,7 @@ cl <- startMPIcluster()  # use verbose = TRUE for detailed worker message output
 registerDoMPI(cl)
 
 system.time(
-    foreach(i = 1:1000) %dopar% sum(sort(runif(1e7)))  # parallel execution
+    foreach(i = 1:100) %dopar% sum(sort(runif(1e7)))  # parallel execution
 )
 
 closeCluster(cl)
