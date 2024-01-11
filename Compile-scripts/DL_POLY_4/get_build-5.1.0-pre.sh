4adef1931469ab6c53ce655a9b18431a03d9103b
Kurt Lust <Kurt.Lust@uantwerpen.be>
2024-01-11 17:10:11 +0100

    Some testing with DL_POLY to prepare EasyConfigs.

---
+++ Compile-scripts/DL_POLY_4/get_build-5.1.0-pre.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/bash
+#
+
+version='5.1.0-pr'
+sourcedir="dl-poly-$version"
+builddir=build-dspoly-cray
+
+module load CrayEnv
+module load cpe/23.09
+module load cpe/23.09
+module load PrgEnv-cray
+module load craype-x86-milan
+
+module load buildtools/23.09
+
+
+git clone -b $version https://gitlab.com/ccp5/dl-poly $sourcedir
+
+#export CMAKE_FC_COMPILER=ftn
+export FC=ftn
+#export F90=ftn
+#export F77=ftn
+export MPIFC=ftn
+#export MPIF90=ftn
+#export MPIF77=ftn
+
+cmake -S $sourcedir -B $builddir -DCMAKE_BUILD_TYPE=Release
+cd $builddir ; make 
