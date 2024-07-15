38ac0904df6b0b9306cb5a2bc0842b2412bf86bb
Kurt Lust <Kurt.Lust@uantwerpen.be>
2024-07-15 18:26:49 +0200

    Cleaning up the GPU tests.

--- GPU/OpenCL/PACKT_saxpy/compile_gnu.sh
+++ GPU/OpenCL/PACKT_saxpy/compile_gnu.sh
@@ -4,4 +4,4 @@ ml PrgEnv-gnu
 ml rocm/5.2.3
 
 export OPENCLROOT=/opt/rocm-5.2.3/opencl
-CC -I$OPENCLROOT/include -L$OPENCLROOT/lib -lOpenCL -craype-verbose saxpy.cpp -o saxpy
+CC -I$OPENCLROOT/include -L$OPENCLROOT/lib -lOpenCL -craype-verbose saxpy.cpp -o saxpy.x
